50fb94fa0c316537a8dd746564559e5dd90d24d7
nachopalmieri <nachopalmieri99@gmail.com>
2022-08-18 21:40:02 -0300

    fix password_reset_email

--- MusicProject/MusicProject/settings.py
+++ MusicProject/MusicProject/settings.py
@@ -140,7 +140,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 2
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
@@ -153,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rafaela.soporte@gmail.com'
-EMAIL_HOST_PASSWORD = 'Soporte123'
+EMAIL_HOST_USER = 'nachopalmieri99@gmail.com'
+EMAIL_HOST_PASSWORD = 'xaldojjuayjjuhlm'
