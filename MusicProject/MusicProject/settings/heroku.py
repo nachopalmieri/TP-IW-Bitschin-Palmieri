e1c0e57623bff9502fd62f4df65e8ea569d56470
ebitschin <ebitschin@folderit.net>
2022-08-23 23:36:51 -0300

    Update heroku settings for https

--- MusicProject/MusicProject/settings/heroku.py
+++ MusicProject/MusicProject/settings/heroku.py
@@ -4,4 +4,7 @@ import django_heroku
 
 # Define heroku specific settings
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 django_heroku.settings(locals())
