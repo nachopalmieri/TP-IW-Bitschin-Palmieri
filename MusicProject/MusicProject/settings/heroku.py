0c8b5a0c4def3fbfb600f26de0d773545567a7bb
ebitschin <ebitschin@folderit.net>
2022-08-26 01:37:10 -0300

    Fix remove https settings

--- MusicProject/MusicProject/settings/heroku.py
+++ MusicProject/MusicProject/settings/heroku.py
@@ -4,7 +4,4 @@ import django_heroku
 
 # Define heroku specific settings
 
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 django_heroku.settings(locals())
