614e1d7260d2c4484c8d555e34327e73af1aedcf
ebitschin <ebitschin@folderit.net>
2022-09-13 11:58:18 -0300

    fix heroku static settings

--- MusicProject/MusicProject/settings/heroku.py
+++ MusicProject/MusicProject/settings/heroku.py
@@ -5,5 +5,6 @@ import django_heroku
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 django_heroku.settings(locals(), logging=False)
